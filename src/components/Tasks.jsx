0b4c974233c21b53a3d5c290094b0f5325bf63ee
Flo Orford <floorford@Flos-MacBook-Pro.local>
2018-06-29 16:03:14 +0100

    styling

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -47,7 +47,7 @@ class Tasks extends Component {
 
     return(//everything. setting the right methods on the right buttons. (methods are functions that live inside an object, and they use . notation)
       <React.Fragment>
-        <form style={{ textAlign: "center" }} onSubmit={ this.handleSubmit }>
+        <form style={{ textAlign: "center", marginBottom: "25px" }} onSubmit={ this.handleSubmit }>
           <label htmlFor="task" style={{ display: "block" }}>Add a Task: </label>
           <input id="task" onChange={ this.handleChange } value={ value }></input>
           <button className="btn btn-outline-success" style={{ margin: "4px" }}>Add</button>
